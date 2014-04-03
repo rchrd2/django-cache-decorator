14b177078c892814107f5250a5dafbc0e5a6ecbb
rcaceres <me@rchrd.net>
2014-04-03 14:27:33 -0700

    Add long readme to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     license = 'MIT',
     version = '0.1',
     description = 'Easily add caching to functions within a django project.',
+    long_description=open('README.md').read(),
     author = 'Richard Caceres',
     author_email = 'me@rchrd.net',
     url = 'https://github.com/rchrd2/django-cache-decorator/',
